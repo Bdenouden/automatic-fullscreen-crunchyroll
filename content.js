2e36975a2e70b80fdbe7ce248278082a534df441
Bram <bramdenouden96@hotmail.com>
2021-01-24 18:48:31 +0100

    wait for element added

--- content.js
+++ content.js
@@ -1,26 +1,32 @@
-window.addEventListener("load", function () {
-	chrome.storage.sync.get(["activated"], ({ activated }) => {
-		if (activated == undefined) {
-			chrome.storage.sync.set({ activated: false });
-		}
+window.addEventListener("load", function() {
+    chrome.storage.sync.get(["activated"], ({ activated }) => {
+        if (activated == undefined) {
+            chrome.storage.sync.set({ activated: false });
+        }
 
-		if (activated) {
-			let elem = document.querySelector("#vilos-player");
+        if (activated) {
+            function waitForElement() {
+                let elem = document.querySelector("#vilos-player");
+                if (elem != null && elem != undefined) {
+                    //variable exists
+                    console.log("requesting fullscreen")
+                    elem.style.position = "fixed";
+                    elem.style.left = "0";
+                    elem.style.top = "0";
 
-			if (elem != null && elem != undefined) {
-				elem.style.position = "fixed";
-				elem.style.left = "0";
-				elem.style.top = "0";
-
-				// Need to hide all elements except #vilos-player
-				document
-					.querySelectorAll("body *:not( #vilos-player )")
-					.forEach((value) => {
-						if (!value.querySelector("#vilos-player")) {
-							value.style.display = "none";
-						}
-					});
-			}
-		}
-	});
+                    // Need to hide all elements except #vilos-player
+                    document
+                        .querySelectorAll("body *:not( #vilos-player )")
+                        .forEach((value) => {
+                            if (!value.querySelector("#vilos-player")) {
+                                value.style.display = "none";
+                            }
+                        });
+                } else {
+                    setTimeout(waitForElement, 250);
+                }
+            }
+            waitForElement()
+        }
+    });
 });
